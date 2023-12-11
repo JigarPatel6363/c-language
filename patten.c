54c286ef89157b57e6b114efda9860841e33e5c3
JigarPatel6363 <152470691+JigarPatel6363@users.noreply.github.com>
2023-12-11 18:54:10 +0530

    Add files via upload

---
+++ patten.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+ int main(){
+    printf("\n\t\t*\t\t*");
+    printf("\n\t*\t\t*");
+    printf("\n*\t\t*");
+    printf("\n\t*\t\t*");
+    printf("\n\t\t*\t\t*");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+
+
+    printf("\n\t*\t*\t*\t*\t*\t*\t*");
+    printf("\n");
+    printf("\n");
+    printf("\n*\t*\t*\t*\t*\t*\t*\t*");
+    printf("\n*\t\t*\t\t\t\t\t*");
+    printf("\n*\t\t*\t\t*\t*\t*\t*");
+    printf("\n*\t\t*\t\t*\t\t*\t*");
+    printf("\n*\t*\t*\t*\t*\t*\t*\t*");
+
+
+
+
+}
+
