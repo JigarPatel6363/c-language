8fc44089dba89ac628b23a6adbefbf7266f461b4
JigarPatel6363 <152470691+JigarPatel6363@users.noreply.github.com>
2023-12-18 13:06:37 +0530

    Add files via upload

---
+++ labwork1/Q1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("\n enter a,b");
+    scanf("%d %d",&a,&b);
+
+    if (a<b)
+    { 
+        printf("\n a is minmum");
+
+        
+    }else{
+        printf("\n b is minmum");
+    }
+    
+}
---
+++ labwork1/Q2.C
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+    int j;
+    printf("\n enter j:");
+    scanf("%d",&j);
+
+    if (j>0){
+           printf("\n is positive on");
+       
+       
+    }else if (j<0){
+        printf("\n is negative on");
+    }else{
+        printf("\n is neutral on");
+    }
+    
+   return 0; 
+}
