dd0fa2203dd1e4865e4ae90c9b6518a95a186be5
JigarPatel6363 <152470691+JigarPatel6363@users.noreply.github.com>
2023-12-25 16:01:35 +0530

    Add files via upload

---
+++ circlet/Q7.C
@@ -0,0 +1,22 @@
+//PTN-7
+
+#include<stdio.h>
+int main()
+{
+    int i, j;
+    
+    for (i = 1; i <= 5; i++)
+    {
+        if (i==1 || i==3)
+        {
+           printf("* * * * *");
+        } else
+        {
+           printf("*       *");
+        }  
+        printf("\n");
+    }
+   
+
+    return 0;
+}
